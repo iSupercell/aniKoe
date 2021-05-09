ebb9fccb76feb2e949acb09b9dd122cc6d82e205
SijiaMao <sijia501@gmail.com>
2021-05-08 23:51:58 -0400

    fixed API call and changed some styling :mag: :clipboard:

--- scripts.js
+++ scripts.js
@@ -13,83 +13,130 @@
 // My codes starts below!!!
 const app = {};
 
+app.apiUrl = `https://api.jikan.moe/v3/`;
 
-// Make a jQuery AJAX request to get info of voice actors
-app.getVc = function(id) {
+// API call to get list of characters from search input
+app.getCharaList = (characterName) => {
     $.ajax({
-        url: `https://api.jikan.moe/v3/person/${id}`,
+        url: `${app.apiUrl}search/character`,
         method: `GET`,
-        dataType: `JSON`
-    }).then(function(data) {
-        // console.log(`vc data`, data);
+        dataType: `JSON`,
+        data: {
+            q: `${characterName}`,
+            genre: 12,
+            genre_exclude: 0,
+            limit: 10,
+        }
+    }).then((data) => {
         $('.aniChara').empty();
-        app.displayVc(data);
+        $('.searchResults').empty();
+        app.displayChara(data);
+    });
+};
+
+// API call to get detailed character info
+app.getChara= (charaId) => {
+    $.ajax({
+        url: `${app.apiUrl}character`,
+        method: `GET`,
+        dataType: `JSON`,
+        data: {
+            id: `${charaId}`
+        }
+    }).then((data) => {
+        // app.displayChara(data);
+        // console.log(data);
     });
 };
 
 
 // Code for displaying data from API
-app.displayVc = function(results) {
+app.displayChara = (results) => {
+    const charaResults = results.results;
+    charaResults.forEach((chara) => {
+        const cleanName = chara.name.replace(`,`, ``);
+        if (chara.image_url != `https://cdn.myanimelist.net/images/questionmark_23.gif` && chara.anime.length > 0) {
+            const charaHtml = `
+            <li>
+                <img src="${chara.image_url}" alt="${cleanName}" tabIndex="0">
+                <div class="charaOverlay">
+                    <p>${cleanName}</p>
+                </div>
+            </li>
+            `;
+            $('.intro').addClass('hide');
+            $('.searchResults').removeClass('hide');
+            $('.searchResults').append(charaHtml);
+        }
+        $('.searchResults li').on('click', () => {
+            console.log(this);
+        });
+    });
+};
+
+app.selectChara = (id) => {
+
+}
+
+// app.displayVc = function(results) {
     // console.log(`this is info`, results);
     
     // Cleaning up Birthday data from API results
-    const editBday = results.birthday;
-    const editedBday = editBday.substr(0, 10);
+    // const editBday = results.birthday;
+    // const editedBday = editBday.substr(0, 10);
 
     // HTML code for voice actor information
-    const vcHtml = `
-    <img src="${results.image_url}" alt="${results.name}">
-    <ul class="vcInfo">
-        <li><h2><i aria-hidden="true" class="far fa-address-card"></i> ${results.name}</h2></li>
-        <li><h3>${results.family_name} ${results.given_name}</h3></li>
-        <li><p>Birthday: ${editedBday}</p></li>
-        <li><p>Total Roles: ${results.voice_acting_roles.length}</p></li>
-        <li><p>MyAnimeList: <a href="${results.url}"> <i class="fas fa-link"></i> Click</a></p></li>
-    </ul>
-    `;
-    $('.vcCard').html(vcHtml);
+    // const vcHtml = `
+    // <img src="${results.image_url}" alt="${results.name}">
+    // <ul class="vcInfo">
+    //     <li><h2><i aria-hidden="true" class="far fa-address-card"></i> ${results.name}</h2></li>
+    //     <li><h3>${results.family_name} ${results.given_name}</h3></li>
+    //     <li><p>Birthday: ${editedBday}</p></li>
+    //     <li><p>Total Roles: ${results.voice_acting_roles.length}</p></li>
+    //     <li><p>MyAnimeList: <a href="${results.url}"> <i class="fas fa-link"></i> Click</a></p></li>
+    // </ul>
+    // `;
+    // $('.vcCard').html(vcHtml);
 
     // Apply filter so that only main character roles will show
-    const rolesArray = results.voice_acting_roles;
-    const onlyMain = rolesArray.filter(function(roleType) {
-        return roleType.role.length == 4;
-    });
+    // const rolesArray = results.voice_acting_roles;
+    // const onlyMain = rolesArray.filter(function(roleType) {
+    //     return roleType.role.length == 4;
+    // });
 
     // HTML code for voice roles (and append to page)
-    onlyMain.forEach(function(role) {
-        // console.log(role);
-        const aniRoles = `
-        <li>
-            <img src="${role.character.image_url}" alt="${role.character.name}">
-            <div class="overlay">
-                <p>Anime:</p>
-                <p class="animeName">${role.anime.name}</p>
-                <p>Character Name</p>
-                <p class="charaName">${role.character.name}</p>
-            </div>
-        </li>
-        `;
-        $('.aniChara').append(aniRoles);
-    });
-};
+    // onlyMain.forEach(function(role) {
+    //     // console.log(role);
+    //     const aniRoles = `
+    //     <li>
+    //         <img src="${role.character.image_url}" alt="${role.character.name}">
+    //         <div class="overlay">
+    //             <p>Anime:</p>
+    //             <p class="animeName">${role.anime.name}</p>
+    //             <p>Character Name</p>
+    //             <p class="charaName">${role.character.name}</p>
+    //         </div>
+    //     </li>
+    //     `;
+    //     $('.aniChara').append(aniRoles);
+    // });
+// };
 
 
 // Code that kicks off the app
 app.init = function() {
-    // console.log('ready');
-    
     // Code for Search input
     $('form').on('submit', function(event) {
         event.preventDefault();
-        const vcSearch = $('input').val();
-        app.getVc(vcSearch);
-        // console.log(`this is an input`, vcSearch)
+        const charaSearch = $('input').val();
+        app.getCharaList(charaSearch);
+        // console.log(`this is an input`, charaSearch)
         $('#searchInput').val('');
     });
 };
 
 
 // Document Ready
-$(function() {
+$(() => {
     app.init();
 });
